7307a6818c6055908fd59fee33031156996e0a06
Sagar Bharti <onename4all.in@gmail.com>
2026-04-18 15:58:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ import os
 import tempfile
 from flask import Flask, request, jsonify
 from faster_whisper import WhisperModel
+from twilio.rest import Client
 
 app = Flask(__name__)
 
 print("Loading model...")
 model = WhisperModel("tiny", device="cpu", compute_type="int8")
 print("Model ready!")
+
+# Twilio config
+TWILIO_SID   = 'AC83bdbc7030aaf272835d920ed5f52bb4'
+TWILIO_TOKEN = 'e9ff220dd5d983d0a3a69f770783443c'
+TWILIO_FROM  = '+12182979590'
+
+# Emergency contacts — jinhe SMS jaayega
+EMERGENCY_CONTACTS = ['+918085055261', '+917987504890']
 
 EMERGENCY_KEYWORDS = [
     "help", "help me", "save me", "emergency",
@@ -22,6 +31,19 @@ def detect_keywords(text):
     text_lower = text.lower().strip()
     found = [kw for kw in EMERGENCY_KEYWORDS if kw in text_lower]
     return len(found) > 0, found
+
+def send_twilio_sms(message):
+    try:
+        client = Client(TWILIO_SID, TWILIO_TOKEN)
+        for number in EMERGENCY_CONTACTS:
+            client.messages.create(
+                body=message,
+                from_=TWILIO_FROM,
+                to=number
+            )
+            print(f"✅ SMS sent to {number}")
+    except Exception as e:
+        print(f"❌ Twilio error: {e}")
 
 @app.route('/health', methods=['GET'])
 def health():
@@ -49,7 +71,7 @@ def analyze():
             segments, info = model.transcribe(
                 tmp_path,
                 language=None,
-                beam_size=1        # kam memory use karega
+                beam_size=1
             )
             transcript = " ".join([s.text for s in segments]).strip()
             detected_lang = info.language
@@ -57,6 +79,17 @@ def analyze():
             print(f"Transcript: '{transcript}' | Language: {detected_lang}")
 
             is_emergency, keywords_found = detect_keywords(transcript)
+
+            # Emergency detect hote hi auto SMS
+            # if is_emergency:
+            #     msg = f"🚨 EMERGENCY ALERT!\nMujhe madad chahiye!\nSuna: {transcript}\n- EmergencyApp"
+            #     send_twilio_sms(msg)
+            #     print("🚨 Emergency triggered! SMS sent.")
+
+            if is_emergency:
+              msg = f"🚨 EMERGENCY ALERT!\nMujhe madad chahiye!\nSuna: {transcript}\n- EmergencyApp"
+              send_twilio_sms(msg)
+              print("🚨 Emergency triggered! SMS sent.")
 
             return jsonify({
                 "emergency": is_emergency,
